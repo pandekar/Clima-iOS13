4e9971687aae8bee44619a193d7220f8d0211efd
pandekar <pandeadhistanaya@gmail.com>
2023-01-24 21:13:56 +0800

    UITextField and UITextFieldDelegate

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -19,10 +19,48 @@ class WeatherViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         searchTextField.placeholder = "Cari"
+        
+        // connect UITextField to UITextFieldDelegate
+        searchTextField.delegate = self
     }
 
     @IBAction func searchPressed(_ sender: UIButton) {
         print(searchTextField.text!)
+        
+        //close keyboard when value is true
+        searchTextField.endEditing(true)
+    }
+    /**
+     UITextField delegate function, function is triggered when keyboard return button is pressed
+     */
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        
+        //close keyboard when value is true
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    
+    /**
+      UITextField delegate function, function is triggered to close the keyboard
+      when the condition inside the function is fulfilled
+     */
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true
+        } else {
+            searchTextField.placeholder = "type something here..."
+            return false
+        }
+    }
+    
+    //UITextField delegate function, function is triggered when done editing the textField
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        // use searchTextField.text to get  the weather for that city.
+        
+        searchTextField.text = ""
     }
 }
 
