c50303a79067809e0018a603097cdc777b6c3ab2
pandekar <pandeadhistanaya@gmail.com>
2023-02-11 15:41:50 +0800

    use the URLSession and networking

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,10 +9,49 @@
 import Foundation
 
 struct WeatherManager {
+
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=01eefb1e65deb044d3ee72e3b5cd5ef3&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+
+        //1. create a URL
+        if let url = URL(string: urlString) {
+            
+            //2. create a URLSession
+            let urlSession = URLSession(configuration: .default)
+            
+            //3. give the session a task
+            //5. resolve completionHandler
+            let task = urlSession.dataTask(with: url, completionHandler: handler(_:_:_:))
+            
+            //4. start the task
+            task.resume()
+        }
+    }
+    
+    //6. make callback function for completionHandler
+    func handler(_ data: Data?, _ urlResponse: URLResponse?, _ error: Error?) {
+        
+        //7. check if there is error
+        if (error != nil) {
+            print(error!)
+            //exit function, do not continue
+            return
+        }
+        
+        //8. process acquired data
+        if let resultData = data {
+            
+            //9. convert type Data to String
+            let dataString = String(data: resultData, encoding: .utf8)
+            print(dataString)
+        }
+        
     }
 }
