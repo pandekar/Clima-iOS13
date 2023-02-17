13140c2a9c202c1e5fdbc716406b0199cc049c88
pandekar <pandeadhistanaya@gmail.com>
2023-02-17 13:44:48 +0800

    make closure for completionHandler

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,17 +9,17 @@
 import Foundation
 
 struct WeatherManager {
-
+    
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=01eefb1e65deb044d3ee72e3b5cd5ef3&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-
+        
         performRequest(urlString: urlString)
     }
     
     func performRequest(urlString: String) {
-
+        
         //1. create a URL
         if let url = URL(string: urlString) {
             
@@ -27,31 +27,20 @@ struct WeatherManager {
             let urlSession = URLSession(configuration: .default)
             
             //3. give the session a task
-            //5. resolve completionHandler
-            let task = urlSession.dataTask(with: url, completionHandler: handler(_:_:_:))
+            //5. make closure for callback <completionHandler: (Data?, URLResponse?, Error?) -> Void>
+            let task = urlSession.dataTask(with: url, completionHandler: { (data, urlResponse, error) in
+                if error != nil {
+                    print(error!)
+                }
+                
+                if let resultData = data {
+                    let dataString = String(data: resultData, encoding: .utf8)
+                    print(dataString!)
+                }
+            })
             
             //4. start the task
             task.resume()
         }
-    }
-    
-    //6. make callback function for completionHandler
-    func handler(_ data: Data?, _ urlResponse: URLResponse?, _ error: Error?) {
-        
-        //7. check if there is error
-        if (error != nil) {
-            print(error!)
-            //exit function, do not continue
-            return
-        }
-        
-        //8. process acquired data
-        if let resultData = data {
-            
-            //9. convert type Data to String
-            let dataString = String(data: resultData, encoding: .utf8)
-            print(dataString)
-        }
-        
     }
 }
